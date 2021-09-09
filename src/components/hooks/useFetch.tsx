14e99fc38cf56a1444a45bbdea31596e863f8854
kaptenPrice <khazar.mehraban@gmail.com>
2021-09-09 20:23:48 +0200

    netlify

--- src/components/hooks/useFetch.tsx
+++ src/components/hooks/useFetch.tsx
@@ -3,8 +3,8 @@ const useFetch: UseFetchType = async (endpoint, init) => {
     try {
         const response = await fetch(`https://miwine.herokuapp.com${endpoint}`, {
             method: 'GET',
-            // headers: { 'Content-Type': 'application/json; charset=UTF-8' },
-            // credentials: 'include',
+            headers: { 'Content-Type': 'application/json; charset=UTF-8' },
+            credentials: 'include',
             ...init,
           
         });
